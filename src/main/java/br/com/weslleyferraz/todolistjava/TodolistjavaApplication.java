219a07a8ce871e21b233d15a16f8257fc30f5bc8
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-10-11 12:36:36 -0300

    A

---
+++ src/main/java/br/com/weslleyferraz/todolistjava/TodolistjavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.weslleyferraz.todolistjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/weslleyferraz/todolistjava/TodolistjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.weslleyferraz.todolistjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
