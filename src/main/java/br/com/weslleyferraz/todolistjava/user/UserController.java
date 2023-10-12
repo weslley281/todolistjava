9db31e26f9bf71f8fef0cdd0e7aa27e99a6749f7
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-10-12 19:16:14 -0400

    a

--- src/main/java/br/com/weslleyferraz/todolistjava/user/UserController.java
+++ src/main/java/br/com/weslleyferraz/todolistjava/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.weslleyferraz.todolistjava.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName() + ' ' + userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if (user != null){
+            System.out.println("Usuário já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
