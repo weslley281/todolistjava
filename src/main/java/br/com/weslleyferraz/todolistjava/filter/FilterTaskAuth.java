c0cf01df633e4d11240da8abc3f80bf4192106a8
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-10-14 06:29:08 -0400

    a

--- src/main/java/br/com/weslleyferraz/todolistjava/filter/FilterTaskAuth.java
+++ src/main/java/br/com/weslleyferraz/todolistjava/filter/FilterTaskAuth.java
@@ -1,16 +1,59 @@
 package br.com.weslleyferraz.todolistjava.filter;
 
+import br.com.weslleyferraz.todolistjava.user.IUserRepository;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
+
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        filterChain.doFilter(request, response);
+        var serveletPath = request.getServletPath();
+        if (serveletPath.equals("/tasks/")) {
+            var authorization = request.getHeader("Authorization");
+
+            System.out.println("Authorazation: " + authorization);
+            var authEncoded = authorization.substring("Basic".length()).trim();
+
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecode);
+
+            System.out.println("AuthEncoded: " + authEncoded);
+            System.out.println("AuthDecoded: " + authDecode);
+            System.out.println("AuthString: " + authString);
+
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            System.out.printf("Usuário: %s, Senha: %s\n", username, password);
+
+            var user = this.userRepository.findByUsername(username);
+
+            if (user == null) {
+                response.sendError(401, "Usuário sem autorização");
+            } else {
+                var passwordVerify = BCrypt.checkpw(password, user.getPassword());
+                if (passwordVerify) {
+                    request.setAttribute("userId", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401, "Usuário sem autorização");
+                }
+            }
+        }else {
+            filterChain.doFilter(request, response);
+        }
     }
 }
--- src/main/java/br/com/weslleyferraz/todolistjava/task/TaskController.java
+++ src/main/java/br/com/weslleyferraz/todolistjava/task/TaskController.java
@@ -1,5 +1,6 @@
 package br.com.weslleyferraz.todolistjava.task;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,14 +10,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
     @Autowired
     private ITaskRepository taskRepository;
     @PostMapping("/")
-    public ResponseEntity create(@RequestBody TaskModel taskModel){
-        System.out.println("Chegou no controller");
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var userId = request.getAttribute("userId");
+
+        System.out.printf("Chegou no controller, id usuário: %s", userId);
+
+        taskModel.setUserId((UUID) userId);
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
